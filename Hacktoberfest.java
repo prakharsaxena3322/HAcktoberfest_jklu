84550250989939cc6d4f973ce0a86f4a5acd0c53
prakharsaxena3322 <56475150+prakharsaxena3322@users.noreply.github.com>
2022-10-04 23:32:30 +0530

    Create Hacktoberfest.java

---
+++ Hacktoberfest.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Hactoberfest{
+  public stativ void main(String [] args){
+    System.out.print("Hello Coders and Hackers");
+    int a = 12, b=3;
+    int c = a*b;
+    System.out.print("Calculation of a multiply by b is: "+c);
+  }
+}
