3ef9e46a6452d9b2ae1a3d6327a1601642c5a84a
prakharsaxena007 <114650147+prakharsaxena007@users.noreply.github.com>
2022-10-19 18:47:56 +0530

    Added function to print the sorted array

--- Quicksort.java
+++ Quicksort.java
@@ -46,6 +46,16 @@ class QuickSort
             sort(arr, pi+1, high); 
         } 
     } 
+    
+      /* A utility function to print array of size n */
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
+  
 
     // Driver program 
     public static void main(String args[]) 
